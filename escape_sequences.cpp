5de26b1b1bba1efc9c31e5d02646e7e331a1b0cc
Amit00199 <Amit.123854@stu.upes.ac.in>
2023-09-15 01:53:37 +0530

    basics

---
+++ escape_sequences.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    cout << "Backspace\b"; //backspaces 1 character - can be repeated
+    cout << "Form feed\f"; //form feed character is a control character that tells the printer to advance to the next page -
+                           //usually used to seperate different sections
+    cout << "Newline\n"; //moves to the next line
+    cout << "Return\r";
+    cout << "Horizontal tab\t";
+    cout << "Vertical tab\v";
+    cout << "Backslash\\";
+    cout << "Single Quotation mark\'";
+    cout << "Double Qotation mark\"";
+    cout << "Question mark\?";
+    cout << "Null character\0";
+    cout << "Different \
+                            Lines";
+
+    return 0;
+}

8fa160b68f9be822c44a939950193c11b96f9ba1
Amit00199 <Amit.123854@stu.upes.ac.in>
2023-09-20 12:59:06 +0530

    added more escape sequences

--- escape_sequences.cpp
+++ escape_sequences.cpp
@@ -9,7 +9,7 @@ int main() {
     cout << "Form feed\f"; //form feed character is a control character that tells the printer to advance to the next page -
                            //usually used to seperate different sections
     cout << "Newline\n"; //moves to the next line
-    cout << "Return\r";
+    cout << "Return\r"; //typically moves the cursor or insertion point to the beginning of the current line
     cout << "Horizontal tab\t";
     cout << "Vertical tab\v";
     cout << "Backslash\\";
@@ -19,6 +19,9 @@ int main() {
     cout << "Null character\0";
     cout << "Different \
                             Lines";
+    cout << "Alarm/Beep Sound\a";
+    cout << "Octal number\101"; //to represent value of an octal number \nnn
+    cout << "Hexadecimal number\x41"; //to represent value of an hexadecimal number \xhh
 
     return 0;
 }
